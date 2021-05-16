d1c462e1c86ba94db0e3f99c67c0079163a8217b
aya berjawi <ayabarjawi@gmail.com>
2021-05-16 17:07:05 +0300

    lab26

---
+++ app/src/main/java/com/example/taskmaster/alltasks.java
@@ -0,0 +1,26 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class alltasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alltasks);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent backArrow =new Intent (getApplicationContext(),MainActivity.class);
+        startActivity(backArrow);
+        return true;
+
+    }
+}
